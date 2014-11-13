0e7b3f14bbecc542fa0fbabd0c4104d30582064a
Kevin Kao <kk415kk@gmail.com>
2014-11-13 01:38:56 -0800

    Added graph version to visualization

--- draw_graph.py
+++ draw_graph.py
@@ -7,19 +7,25 @@ G = LB.DynamicGraph()
 a = LB.Node('A')
 b = LB.Node('B')
 c = LB.Node('C')
+d = LB.Node('D')
 e1 = LB.Edge(a,b)
 e2 = LB.Edge(a,c)
 e3 = LB.Edge(c,a)
 e4 = LB.Edge(b,c)
+e5 = LB.Edge(c,d)
+e6 = LB.Edge(d,c)
 edge_set = set([e1, e2, e3])
 G.insert(edge_set)
 G.insert(set([e4]))
+G.insert(set([e5]))
+G.insert(set([e6]))
 
 # Draw the graph and generate
 graph = pydot.Dot('Forest')
 for node in G.parent:
-  if node != G.parent[node]:
-    edge = pydot.Edge(str(G.parent[node]), str(node))
-    graph.add_edge(edge)
+  f_node = str(G.parent[node]) + " - " + str(G.version[G.parent[node]])
+  s_node = str(node) + " - " + str(G.version[node])
+  edge = pydot.Edge(f_node, s_node)
+  graph.add_edge(edge)
 graph.write_png('graph.png')
 
--- test.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-from graph import *
-test()
